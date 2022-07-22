cdd031f55f1039ffe3fb4e9b2091c4fef58a136a
Alban <renier.alban@gmail.com>
2022-07-22 16:46:42 +0200

    etat hook UseState - etat des icones likeMovie et watchMovie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,9 @@
-import React from "react";
-import '../App.css';
+import React, { useState } from "react";
+import "../App.css";
 
 // Import des composants ReactStrap
 import {
-Button,
+  Button,
   Col,
   Card,
   CardImg,
@@ -24,18 +24,34 @@ import { faHeart, faVideo, faStar } from "@fortawesome/free-solid-svg-icons";
 // Debut de la fonction return React du composant Movie.js
 
 function Movie(props) {
+  // Initialisation d'un hook d'etat pour likeMovie
+  const [likeMovie, setLikeMovie] = useState(false);
+  // Initialisation d'un hook d'etat pour watchMovie
+  const [watchMovie, setWatchMovie] = useState(false);
 
-    // Dynamiser le nombre d'etoiles
-    var tabGlobalRating =[];
-        for(var i=0; i<10; i++){
-            var color={}
-            if(i<props.globalRating){
-                color = { color: '#e9eb19' };
-            }
-            tabGlobalRating.push(
-              <FontAwesomeIcon style={color} icon={faStar} />
-            );
-        }
+  // Dynamiser le nombre d'etoiles pour la moyenne
+  var tabGlobalRating = [];
+  for (var i = 0; i < 10; i++) {
+    var color = {};
+    if (i < props.globalRating) {
+      color = { color: "#e9eb19" };
+    }
+    tabGlobalRating.push(<FontAwesomeIcon style={color} icon={faStar} />);
+  }
+  // Condition if de likeMovie - Change la couleur de l'icone coeur
+  if (likeMovie) {
+    var colorLike = { color: "#FFDA22", cursor: "pointer" };
+  } else {
+    var colorLike = { color: "#DDDBF1", cursor: "pointer" };
+  }
+  // Condition if de watchMovie - Change la couleur de l'icone watch
+
+if (watchMovie) {
+  var colorWatch = { color: "#FFDA22", cursor: "pointer" };
+} else {
+  var colorWatch = { color: "#DDDBF1", cursor: "pointer" };
+}
+
   return (
     <Col xs="12" lg="6" xl="4">
       <Card style={{ marginBottom: 30 }}>
@@ -43,10 +59,19 @@ function Movie(props) {
         <CardBody>
           <p>
             Like{" "}
-            <FontAwesomeIcon icon={faHeart} style={{ cursor: "pointer" }} />
+            <FontAwesomeIcon
+              style={colorLike}
+              icon={faHeart}
+              onClick={() => setLikeMovie(!likeMovie)}
+            />
           </p>
           <p>
-            Nombre de vues <FontAwesomeIcon icon={faVideo} />{" "}
+            Nombre de vues{" "}
+            <FontAwesomeIcon
+            style={colorWatch}
+              icon={faVideo}
+              onClick={() => setWatchMovie(!watchMovie)}
+            />{" "}
             <Badge color="secondary">2</Badge>
           </p>
           <p>
